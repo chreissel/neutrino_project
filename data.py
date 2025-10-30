6dead8e37164105ba61fd76e316f1cee193aa9f9
lfkoe <domingue3@grinnell.edu>
2025-10-30 12:23:13 -0400

    added ts+fft

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ from utils import fft_func_IQ_abs, fft_func_IQ_complex_channels
 
 class Project8Sim(Dataset):
     def __init__(self, inputs, variables, observables, path='/gpfs/gibbs/pi/heeger/hb637/ssm_files_pi_heeger/combined_data_fullsim.hdf5', cutoff=4000, norm=True, noise_const=1,
-            apply_filter = False, apply_fft=False, complex_channels=False):
+            apply_filter = False, apply_fft=False, complex_channels=False, ts_and_fft=False):
         arr = {}
         with h5py.File(path, 'r') as f:
             for i in inputs+variables+observables:
@@ -21,52 +21,54 @@ class Project8Sim(Dataset):
         obs = np.concatenate([arr[o] for o in observables], axis = 1)
         # add noise and normalize by std
 
-        if ('output_ts_I' in inputs) and ('output_ts_Q' in inputs) and apply_fft:
-            index_ts_I = inputs.index('output_ts_I')
-            index_ts_Q = inputs.index('output_ts_Q')
-            for i in range(X.shape[0]):
-                for j in range(X.shape[2]):
-                    noise_arr = noise_model(cutoff, noise_const)
-                    X_noise = X[i, :, j] + noise_arr
-                    if(apply_filter): # whether to apply the band pass filter
-                        X_noise = bandpass_filter(X_noise)
-                    if(norm): # whether to normalize by std
-                        std_X = np.std(X_noise)
-                    else:
-                        std_X = 1
-                    X[i, :, j] = X_noise/std_X
-
-                if(complex_channels):
-                    X[i,:,index_ts_I], X[i,:,index_ts_Q] = fft_func_IQ_complex_channels(X[i, :, index_ts_I], X[i, :, index_ts_Q])
+        X_ts = np.zeros_like(X)
+        for i in range(X.shape[0]):
+            for j in range(X.shape[2]):
+                noise_arr = noise_model(cutoff, noise_const)
+                X_noise = X[i, :, j] + noise_arr
+                if apply_filter:
+                    X_noise = bandpass_filter(X_noise)
+                if norm:
+                    std_X = np.std(X_noise)
                 else:
-                    X[i,:,0] = fft_func_IQ_abs(X[i, :, index_ts_I], X[i, :, index_ts_Q])
+                    std_X = 1
+                X_ts[i, :, j] = X_noise / std_X
 
+        X_fft = None
+        if apply_fft or ts_and_fft:
+            if ('output_ts_I' in inputs) and ('output_ts_Q' in inputs):
+                index_ts_I = inputs.index('output_ts_I')
+                index_ts_Q = inputs.index('output_ts_Q')
+                X_fft = np.zeros_like(X_ts)
 
-            if apply_fft and not(complex_channels):
-                X = X[:,:,0]
-                X = X[:, :, np.newaxis]
-
-        else:
-            for i in range(X.shape[0]):
-                for j in range(X.shape[2]):
-                    noise_arr = noise_model(cutoff, noise_const)
-                    X_noise = X[i, :, j] + noise_arr
-                    if(apply_filter): # whether to apply the band pass filter
-                        X_noise = bandpass_filter(X_noise)
-                    if(apply_fft):
-                        X_noise = fft_func(X_noise)
-                        if len(X_noise) < cutoff:
-                            X_noise = np.pad(X_noise, (0, cutoff - len(X_noise)), mode='constant')
-                    if(norm and not(apply_fft)): # whether to normalize by std
-                        std_X = np.std(X_noise)
+                for i in range(X.shape[0]):
+                    if complex_channels:
+                        real_part, imag_part = fft_func_IQ_complex_channels(
+                            X_ts[i, :, index_ts_I],
+                            X_ts[i, :, index_ts_Q]
+                        )
+                        X_fft[i, :, index_ts_I] = real_part
+                        X_fft[i, :, index_ts_Q] = imag_part
                     else:
-                        std_X = 1
-                    X[i, :, j] = X_noise/std_X
+                        abs_fft = fft_func_IQ_abs(
+                            X_ts[i, :, index_ts_I],
+                            X_ts[i, :, index_ts_Q]
+                        )
+                        X_fft[i, :, 0] = abs_fft
+                if not complex_channels:
+                    X_fft = X_fft[:, :, [0]]
+
+        if ts_and_fft:
+            X = np.concatenate([X_ts, X_fft], axis=2)
+        elif apply_fft:
+            X = X_fft
+        else:
+            X = X_ts
 
         if norm:
             mu_y = np.mean(y, axis=0)
             stds_y = np.std(y, axis=0)
-            y = (y-mu_y)/stds_y
+            y = (y - mu_y) / stds_y
 
         self.mu = mu_y
         self.stds = stds_y
@@ -100,10 +102,10 @@ class GenericDataModule(L.LightningDataModule):
                               "pin_memory":self.pin_memory}
 
 class LitDataModule(GenericDataModule):
-    def __init__(self, inputs, variables, observables, cutoff=4000, path='/gpfs/gibbs/pi/heeger/hb637/ssm_files_pi_heeger/combined_data_fullsim.hdf5', norm=True, noise_const=1, apply_filter=False, apply_fft=False, complex_channels=False, **kwargs):
+    def __init__(self, inputs, variables, observables, cutoff=4000, path='/gpfs/gibbs/pi/heeger/hb637/ssm_files_pi_heeger/combined_data_fullsim.hdf5', norm=True, noise_const=1, apply_filter=False, apply_fft=False, complex_channels=False, ts_and_fft=False, **kwargs):
         super().__init__(**kwargs)
        
-        dataset = Project8Sim(inputs, variables, observables, path, cutoff, norm, noise_const, apply_filter, apply_fft, complex_channels)
+        dataset = Project8Sim(inputs, variables, observables, path, cutoff, norm, noise_const, apply_filter, apply_fft, complex_channels, ts_and_fft)
         self.mu = dataset.mu
         self.stds = dataset.stds
         generator = torch.Generator().manual_seed(42)
--- submit_luis.sh
+++ submit_luis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=gpu
-#SBATCH --time=02:30:00
+#SBATCH --time=20:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=128G
+#SBATCH --mem=300G
 #SBATCH --output=slurm_logs/output-%j.out
 #SBATCH --error=slurm_logs/error-%j.err
 
