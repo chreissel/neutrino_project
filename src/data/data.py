1a7595a98a04e2771716f99a8ad9b995cd3087e4
Christina Reissel <christina.reissel@cern.ch>
2026-02-09 15:25:17 -0500

    clean up #4: save code structure after testing

--- src/data/data.py
+++ src/data/data.py
@@ -1,89 +1,9 @@
 import lightning as L
-from torch.utils.data import random_split, DataLoader, Dataset
+from torch.utils.data import random_split, DataLoader
 import h5py
 import numpy as np
 import torch
-from noise import *
-from utils import fft_func_IQ_complex_channels
-
-class Project8Sim(Dataset):
-    def __init__(self, inputs, variables, observables, path='/gpfs/gibbs/pi/heeger/hb637/ssm_files_pi_heeger/combined_data_fullsim.hdf5', cutoff=4000, norm=True, noise_const=1,
-            apply_filter = False):
-
-        arr = {}
-        with h5py.File(path, 'r') as f:
-            for i in inputs+variables+observables:
-                arr[i] = f[i][:]
-                arr[i] = arr[i][:, np.newaxis]
-
-        X_original = np.concatenate([arr[i] for i in inputs], axis = 1)
-        self.X_original = np.swapaxes(X_original,1,2)[:,:cutoff, :]
-        self.y_original = np.concatenate([arr[v] for v in variables], axis = 1)
-        self.obs = np.concatenate([arr[o] for o in observables], axis = 1)
-
-        self.inputs = inputs
-        self.cutoff = cutoff
-        self.norm = norm
-        self.apply_filter = apply_filter
-        self.noise_const = noise_const
-
-        if norm:
-            self.mu = np.mean(self.y_original, axis=0)
-            self.stds = np.std(self.y_original, axis=0)
-            self.vars = (self.y_original - self.mu) / self.stds
-        else:
-            self.mu = np.zeros_like(self.y_original[0])
-            self.stds = np.ones_like(self.y_original[0])
-            self.vars = self.y_original
-
-    def __getitem__(self,idx):
-        X_clean = self.X_original[idx].copy()
-        X_ts = X_clean.copy()
-
-        for j in range(X_ts.shape[1]):
-            noise_arr = noise_model(self.cutoff, self.noise_const)
-            X_noise = X_clean[:, j] + noise_arr
-
-            if self.apply_filter:
-                X_noise = bandpass_filter(X_noise)
-
-            std_X = np.std(X_noise) if self.norm else 1
-            X_ts[:, j] = X_noise / std_X
-       
-        index_ts_I = self.inputs.index('output_ts_I')
-        index_ts_Q = self.inputs.index('output_ts_Q')
-        X_fft = np.zeros_like(X_ts)
-
-        real_part, imag_part = fft_func_IQ_complex_channels(
-                    X_ts[:, index_ts_I],
-                    X_ts[:, index_ts_Q] 
-            )
-                
-        fft_case_data = np.stack([real_part, imag_part], axis=1)
-        mu_fft_case = np.mean(fft_case_data, axis=0)
-        stds_fft_case = np.std(fft_case_data, axis=0)
-        real_part_norm = (real_part - mu_fft_case[0]) / stds_fft_case[0]
-        imag_part_norm = (imag_part - mu_fft_case[1]) / stds_fft_case[1]
-        X_fft[:, index_ts_I] = real_part_norm
-        X_fft[:, index_ts_Q] = imag_part_norm
-
-        ts = torch.tensor(X_ts, dtype=torch.float32)
-        fft = torch.tensor(X_fft, dtype=torch.float32)
-        var = torch.tensor(self.vars[idx], dtype=torch.float32)
-        obs = torch.tensor(self.obs[idx,:], dtype=torch.float32)
-        return ts, fft, var, obs
-
-    def outdim(self):
-        return self.vars.shape[1]
-
-    def __len__(self):
-        return self.vars.shape[0]
-
-    def __indim_ts__(self):
-        return self.X_original.shape[2]
-
-    def __indim_fft__(self):
-        return self.X_original.shape[2]
+from src.data.dataset import Project8Sim
 
 class GenericDataModule(L.LightningDataModule):
     def __init__(self,batch_size=512,num_workers=4,pin_memory=False):
@@ -96,15 +16,27 @@ class GenericDataModule(L.LightningDataModule):
                               "pin_memory":self.pin_memory}
 
 class LitDataModule(GenericDataModule):
-    def __init__(self, inputs, variables, observables, cutoff=4000, path='/gpfs/gibbs/pi/heeger/hb637/ssm_files_pi_heeger/combined_data_fullsim.hdf5', norm=True, noise_const=1, apply_filter=False, 
-            use_curriculum_learning=False, max_noise_const=1.0, **kwargs):
+    def __init__(self, 
+            inputs, variables, observables, 
+            cutoff=4000, 
+            path='/gpfs/gibbs/pi/heeger/hb637/ssm_files_pi_heeger/combined_data_fullsim.hdf5', 
+            norm=True, 
+            noise_const=1, 
+            apply_filter=False, 
+            use_curriculum_learning=False, 
+            max_noise_const=1.0,
+            freq_transform = 'fft',
+            q_params = None,
+            **kwargs):
         super().__init__(**kwargs)
         
         initial_noise_const = noise_const
         self.use_curriculum_learning = use_curriculum_learning
 
-        dataset = Project8Sim(inputs, variables, observables, path, cutoff, norm, initial_noise_const, apply_filter)
-        self.dataset = dataset
+        self.dataset = Project8Sim(
+            inputs, variables, observables, path, cutoff, norm,
+            initial_noise_const, apply_filter, freq_transform, q_params
+        )
         self.mu = self.dataset.mu
         self.stds = self.dataset.stds
         generator = torch.Generator().manual_seed(42)
@@ -113,7 +45,9 @@ class LitDataModule(GenericDataModule):
         self.variables = variables
         self.input_channels_ts = self.dataset.__indim_ts__()
         self.input_channels_fft = self.dataset.__indim_fft__()
-        self.save_hyperparameters(ignore=['use_curriculum_learning'])
+        self.input_channels_qtransform = self.dataset.__indim_qtransform__()
+        
+        self.save_hyperparameters()
 
     def set_noise_const(self, new_const):
         if self.use_curriculum_learning:
--- src/utils/transforms.py
+++ src/utils/transforms.py
@@ -25,3 +25,45 @@ def fft_func_IQ_complex_channels(signal_I, signal_Q):
     real_part = yf.real
     imag_part = yf.imag
     return real_part, imag_part
+
+def q_transform_fast(signal, fs, fmin, fmax, q_value=5.0, num_freqs=100):
+    
+    n = len(signal)
+    times = np.arange(n) / fs
+    frequencies = np.logspace(np.log10(fmin), np.log10(fmax), num_freqs)
+
+    q_transform_result = np.zeros((n, num_freqs), dtype=complex)
+
+    for i, f0 in enumerate(frequencies):
+        sigma_t = q_value / (2 * np.pi * f0)
+        window_samples = int(6 * sigma_t * fs)
+        window_samples = min(window_samples, n)
+
+        # Create wavelet
+        t_window = np.arange(-window_samples//2, window_samples//2) / fs
+        window = np.exp(-0.5 * (t_window / sigma_t)**2)
+        window /= np.sqrt(np.sum(window**2))
+        carrier = np.exp(-2j * np.pi * f0 * t_window)
+        wavelet = window * carrier
+
+        # FFT-based convolution
+        signal_fft = fft(signal, n=n + len(wavelet) - 1)
+        wavelet_fft = fft(np.conj(wavelet[::-1]), n=n + len(wavelet) - 1)
+        convolved = ifft(signal_fft * wavelet_fft)
+
+        # Extract valid portion
+        q_transform_result[:, i] = convolved[len(wavelet)//2:len(wavelet)//2 + n]
+
+    return q_transform_result, frequencies, times
+
+
+def qtransform_func_IQ_complex_channels(ts_I, ts_Q, fs=200e6, fmin=1e6, fmax=100e6, 
+                                        q_value=5.0, num_freqs=100):
+    complex_signal = ts_I + 1j * ts_Q
+    q_result, frequencies, times = q_transform_fast(
+        complex_signal, fs, fmin, fmax, q_value, num_freqs
+    )
+    real_part = np.real(q_result).flatten()
+    imag_part = np.imag(q_result).flatten()
+    
+    return real_part, imag_part
